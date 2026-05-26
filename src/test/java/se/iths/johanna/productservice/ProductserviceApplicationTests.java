803c58661ff9e85b2b7d238824d2baef1eb64516
Johannavii <jh.viippa@gmail.com>
2026-05-26 19:31:33 +0200

    Fixat test: Exkluderat securityConfig

--- src/test/java/se/iths/johanna/productservice/ProductserviceApplicationTests.java
+++ src/test/java/se/iths/johanna/productservice/ProductserviceApplicationTests.java
@@ -1,9 +1,12 @@
 package se.iths.johanna.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import se.iths.johanna.productservice.config.SecurityConfig;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = SecurityConfig.class)
 class ProductserviceApplicationTests {
 
     @Test
