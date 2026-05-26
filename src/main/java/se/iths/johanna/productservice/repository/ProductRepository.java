484e9f5ceca111387c98891564888a91277b42bb
Johannavii <jh.viippa@gmail.com>
2026-05-26 11:22:05 +0200

    Fyllt i allt korrekt!

--- src/main/java/se/iths/johanna/productservice/repository/ProductRepository.java
+++ src/main/java/se/iths/johanna/productservice/repository/ProductRepository.java
@@ -1,9 +1,8 @@
 package se.iths.johanna.productservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import se.iths.johanna.productservice.model.Product;
 
-// import se.iths.johanna.productservice.model.Product;
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-public interface ProductRepository extends JpaRepository {
-    // Lägg till <Product, Long>
 }
--- src/main/java/se/iths/johanna/productservice/service/ProductService.java
+++ src/main/java/se/iths/johanna/productservice/service/ProductService.java
@@ -1,12 +1,14 @@
 package se.iths.johanna.productservice.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import se.iths.johanna.productservice.dto.OrderRequestItem;
+import se.iths.johanna.productservice.dto.ProductInfo;
+import se.iths.johanna.productservice.dto.ProductRequestDto;
+import se.iths.johanna.productservice.dto.ProductResponseDto;
 import se.iths.johanna.productservice.repository.ProductRepository;
 
-// import se.iths.johanna.productservice.dto.ProductRequestDto;
-// import se.iths.johanna.productservice.dto.ProductResponseDto;
-// import se.iths.johanna.productservice.dto.ProductInfo;
-// import se.iths.johanna.productservice.dto.OrderRequestItem;
+import java.util.List;
 
 // import se.iths.johanna.productservice.model.Product;
 
@@ -14,32 +16,35 @@ import se.iths.johanna.productservice.repository.ProductRepository;
 public class ProductService {
 
     // Variabel
-    private final ProductRepository productRepository;
+    private final ProductRepository repository;
 
     // Konstruktor
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
     }
 
     // Metod - Skapa produkt
-    //public ProductResponseDto createProduct(ProductRequestDto dto) {
-    // }
+    public ProductResponseDto createProduct(ProductRequestDto dto) {
+        return null;
+    }
 
     // Metod - Hämta alla produkter
-    // public List <ProductResponseDto> getAllProducts() {
-    // }
+    public List<ProductResponseDto> getAllProducts() {
+        return null;
+    }
 
     // Metod - Hämta produkt (id)
-    // public ProductResponseDto getProductById(Long id) {
-    // }
+    public ProductResponseDto getProductById(Long id) {
+        return null;
+    }
 
     // Metod - Ta bort produkt
-    // public void deleteProduct (Long id) {
-    // }
+    public void deleteProduct(Long id) {
+    }
 
     // Metod - Minska antal produkter (order)
-    // @Transactional
-    //public List <ProductInfo> decreaseStock (List<OrderRequestItem> items) {
-    // }
-
+    @Transactional
+    public List<ProductInfo> decreaseStock(List<OrderRequestItem> items) {
+        return null;
+    }
 }
