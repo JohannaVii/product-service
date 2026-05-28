6248f9a6c8ec92108fa2d108342471a12e99fd0e
Johannavii <jh.viippa@gmail.com>
2026-05-28 07:49:47 +0200

    Fixat ProductRepository: -> existsById. CRUD-branchen färdig och i sync med allt.

--- src/main/java/se/iths/johanna/productservice/repository/ProductRepository.java
+++ src/main/java/se/iths/johanna/productservice/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import se.iths.johanna.productservice.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    boolean existById(Long id);
+    boolean existsById(Long id);
 }
